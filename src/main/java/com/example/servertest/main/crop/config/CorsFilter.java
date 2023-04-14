54dcc9b75125414e27acc36e313f601ff0a3faa2
LeeFe49 <chullee49@naver.com>
2023-04-14 20:27:15 +0900

    feat : CORS Filter 설정 33

--- src/main/java/com/example/servertest/main/crop/config/CorsFilter.java
+++ src/main/java/com/example/servertest/main/crop/config/CorsFilter.java
@@ -1,25 +1,35 @@
-//package com.example.servertest.main.crop.config;
-//
-//import jakarta.servlet.Filter;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.ServletRequest;
-//import jakarta.servlet.ServletResponse;
-//import jakarta.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class CorsFilter implements Filter {
-//
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//        throws IOException, ServletException {
-//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-//        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-//        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//        httpServletResponse.setHeader("Access-Control-Allow-Headers", "*");
-//        chain.doFilter(request, response);
-//    }
-//}
-//
+package com.example.servertest.main.crop.config;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods","*");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else {
+            chain.doFilter(req, res);
+        }
+    }
+}
+
