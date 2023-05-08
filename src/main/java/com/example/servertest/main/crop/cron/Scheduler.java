1c7f493febc99921b7e400a60b26f9d32b9ac236
LeeFe49 <chullee49@naver.com>
2023-05-08 18:44:59 +0900

    style : 코드 수정

--- src/main/java/com/example/servertest/main/crop/cron/Scheduler.java
+++ src/main/java/com/example/servertest/main/crop/cron/Scheduler.java
@@ -11,7 +11,7 @@ public class Scheduler {
 
     private final CrawlingService crawlingService;
 
-    @Scheduled(cron = "0 0 0 ? * MON *")
+    @Scheduled(cron = "0 0 0 ? * MON")
     public void saveNotice() {
 
         String idx = crawlingService.getUrl(crawlingService.getIndex());
