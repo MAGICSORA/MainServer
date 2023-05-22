5382d0a9a2a8990bcd78df15f3f5eda513e543c7
LeeFe49 <chullee49@naver.com>
2023-05-22 19:11:26 +0900

    style : 코드수정

--- src/main/java/com/example/servertest/main/crop/exception/CropError.java
+++ src/main/java/com/example/servertest/main/crop/exception/CropError.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CropError {
 
-    DISEASE_NOT_FOUND("존재하지 않는 질병입니다."),
-    FAILED_TO_GET_DATA("데이터를 가져오지 못했습니다.");
+    DISEASE_NOT_FOUND("존재하지 않는 질병입니다.")
+    , FAILED_TO_GET_DATA("데이터를 가져오지 못했습니다.");
 
     private final String description;
 }
