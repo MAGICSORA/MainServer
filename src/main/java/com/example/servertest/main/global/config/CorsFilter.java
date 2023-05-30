980aa3caafad170cf2ef0f6c6c04362ebd3336e4
LeeFe49 <chullee49@naver.com>
2023-05-31 05:47:59 +0900

    feat : CORS

--- src/main/java/com/example/servertest/main/global/config/CorsFilter.java
+++ src/main/java/com/example/servertest/main/global/config/CorsFilter.java
@@ -7,7 +7,9 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,14 +22,14 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
-        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
-        }else {
+        } else {
             chain.doFilter(req, res);
         }
     }
